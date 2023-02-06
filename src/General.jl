7289f907f89bcfe24ffabd212824c78e20ea82f7
TimVWese <tim.vanwesemael@ugent.be>
2023-02-06 11:23:40 +0100

    Formatter

--- src/General.jl
+++ src/General.jl
@@ -29,25 +29,25 @@ function count_states(es, i, s)
 end
 
 function Ie!(e, v_s, v_d, p, t)
-    e .= v_s;
+    e .= v_s
 end
 
-IStaticEdge = dim -> StaticEdge(f = Ie!, dim = dim, coupling=:undirected);
+IStaticEdge = dim -> StaticEdge(f=Ie!, dim=dim, coupling=:undirected)
 
 function SStaticEdge(selection; invalid=0)
     dim = length(selection)
     f = (e, v_s, v_d, p, t) -> begin
         for i in eachindex(v_s)
             e[i] = selection[i] ? v_s[i] : invalid
-        end    
+        end
     end
-    return StaticEdge(f = f, dim=dim, coupling=:undirected)
+    return StaticEdge(f=f, dim=dim, coupling=:undirected)
 end
 
 function combine_graphs(gs::AbstractGraph...)
     dim = nv(gs[1])
     combined_graph = Graph(dim)
-    for g in gs 
+    for g in gs
         @assert dim == nv(g) "Graphs have different number of vertices"
         for edge in edges(g)
             add_edge!(combined_graph, edge)
@@ -63,46 +63,46 @@ end
 
 function combine_graphs(v_f::Function, gs::AbstractGraph...)
     static_edges, combined_graph = combine_graphs(gs...)
-    ode_vertices = [v_f([degree(g, v) for g in gs]) for v in vertices(combined_graph)] 
+    ode_vertices = [v_f([degree(g, v) for g in gs]) for v in vertices(combined_graph)]
     return ode_vertices, static_edges, combined_graph
 end
 
 function majority_vertex(i::Integer, dim::Integer; U=1, A=2)
     f = (vₙ, v, es, p, t) -> begin
-        vₙ .= v;
-        vₙ[i] = count_states(es, i, A)/length(es) >= p.Θ ? A : U
+        vₙ .= v
+        vₙ[i] = count_states(es, i, A) / length(es) >= p.Θ ? A : U
     end
-    return ODEVertex(f = f, dim = dim);
+    return ODEVertex(f=f, dim=dim)
 end
 
 function majority_termination(u, t, integrator)
-    t < 3 ? false : u == integrator.sol(t-2)
+    t < 3 ? false : u == integrator.sol(t - 2)
 end
 
 majority_termination_cb = DiscreteCallback(majority_termination, terminate!)
 
 function SF_configuration_model(N, γ; min_d=1)
     v = min_d:(N-1)
-    v = v.^(-1.0*γ)
+    v = v .^ (-1.0 * γ)
     v ./= sum(v)
 
     dist = Categorical(v)
     samples = zeros(Int64, N)
     samples[1] = 1
     while !isgraphical(samples)
-        samples = rand(dist, N) .+ (min_d-1)
+        samples = rand(dist, N) .+ (min_d - 1)
     end
     return random_configuration_model(N, samples)
 end
 
-function spatial_network(ps::Matrix; f=x->x)
+function spatial_network(ps::Matrix; f=x -> x)
     N = size(ps, 1)
     g = Graph(N)
     maxd = norm(ones(size(ps, 2)))
     for (i1, r1) in enumerate(eachrow(ps))
         for i2 in i1+1:N
-            r2 = ps[i2,:]
-            d = norm(r1 - r2)/maxd
+            r2 = ps[i2, :]
+            d = norm(r1 - r2) / maxd
             if rand() > f(d)
                 add_edge!(g, i1, i2)
             end
@@ -111,21 +111,21 @@ function spatial_network(ps::Matrix; f=x->x)
     return g
 end
 
-function spatial_network(N, d=2; f=x->x)
+function spatial_network(N, d=2; f=x -> x)
     ps = rand(N, d)
     return spatial_network(ps; f=f), ps
 end
 
 function read_from_mtx(filename)
     if length(filename) < 4 || filename[end-3:end] != ".mtx"
-        filename = filename*".mtx"
+        filename = filename * ".mtx"
     end
     return SimpleGraph(mmread(filename))
 end
 
 function write_to_mtx(filename, g::AbstractGraph)
     if length(filename) < 4 || filename[end-3:end] != ".mtx"
-        filename = filename*".mtx"
+        filename = filename * ".mtx"
     end
     mmwrite(filename, adjacency_matrix(g))
 end
